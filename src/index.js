f9b33b717df83304144ade3636a80ffbbcfd5ee4
xedter20 <dextermiranda441@gmail.com>
2025-10-30 16:52:17 +0800

    fixes

--- src/index.js
+++ src/index.js
@@ -52,14 +52,6 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: 'http://localhost:3000' }));
-
-app.use(
-  helmet({
-    crossOriginResourcePolicy: { policy: 'cross-origin' }
-  })
-);
-
 // Serve static files
 app.use('/uploads', express.static('uploads'));
 
